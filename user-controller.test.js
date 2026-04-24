ccb402f58bc7b3071854f08d312b84662846d8bc
mausquerramienta <mauricio.ortega.bm@gmail.com>
2026-04-24 17:23:17 -0600

    modified user-controlles.test.js

--- user-controller.test.js
+++ user-controller.test.js
@@ -13,13 +13,13 @@ test('remove user to userController', () => {
     let user = new User(1234,"Santiago", "santiago@generation.org");
     userController.add(user);    
     userController.remove(user);
-    expect(userController.users).not.toContain(user);
+    expect(userController.getUsers()).not.toContain(user);
   });
 
 test('verify an user not added in userController', () => {
     const userController = new UserController();
     let user = new User(6600, "Mauricio", "mauricio@generation.org");
-    expect(userController.users).not.toContain(user);
+    expect(userController.getUsers()).not.toContain(user);
   })
 
 test('verify remove() removing an user not in userController', () => {
